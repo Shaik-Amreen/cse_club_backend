34ede269adb1d93b141756abe05b0321e7e524e9
Shaik-Amreen <69184237+Shaik-Amreen@users.noreply.github.com>
2022-12-03 12:24:36 +0530

    build

--- index.js
+++ index.js
@@ -29,13 +29,15 @@ app.use(function (req, res, next) {
     next()
 })
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+
 app.use("/", users)
 app.use("/task", task)
 app.use("/submission", submission)
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-})
 
 
 
